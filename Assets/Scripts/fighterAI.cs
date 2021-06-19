7d841dd9baa4569524e5a0d99e78eaeb0c6d655f
Denys Ashikhin <denysashikhin@gmail.com>
2021-06-19 16:31:25 -0400

    added 1v_16 arena where AI is rewarded for longer episodes, also made actions to be requested /frame (up from /2frames)

--- Assets/Scripts/fighterAI.cs
+++ Assets/Scripts/fighterAI.cs
@@ -180,9 +180,8 @@ public class fighterAI : Agent
     }
 
     public override void OnActionReceived(ActionBuffers actions)
-    {
-
-        AddReward((-1f / (float)MaxStep)); // Slowly punishing longer fighting times
+    { 
+        AddReward((1f / (float)MaxStep)); // Slowly punishing longer fighting times
 
         ActionSegment<int> vectorAction = actions.DiscreteActions;
 
